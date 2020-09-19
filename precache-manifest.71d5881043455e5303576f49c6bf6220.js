1a23645b124c2c7782247bf53e59af0e182b1416
z-igor <z-igor@nomail.com>
2020-09-19 10:40:28 +0300

    deploy TRLogic

---
+++ precache-manifest.71d5881043455e5303576f49c6bf6220.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de995a0b191416ffcbc",
+    "url": "/vue-widget_gh-ps/css/app.5c4b88ac.css"
+  },
+  {
+    "revision": "3516f2c333ae6f5877e72cd47de14ae7",
+    "url": "/vue-widget_gh-ps/img/bg.3516f2c3.png"
+  },
+  {
+    "revision": "542cebfa7a22d71eba50dc8d738d298d",
+    "url": "/vue-widget_gh-ps/index.html"
+  },
+  {
+    "revision": "6de995a0b191416ffcbc",
+    "url": "/vue-widget_gh-ps/js/app.a6260d45.js"
+  },
+  {
+    "revision": "36fa33f8e27a7aa2d726",
+    "url": "/vue-widget_gh-ps/js/chunk-2d217e3c.791ec952.js"
+  },
+  {
+    "revision": "81705f35ff9c47a7b1fa",
+    "url": "/vue-widget_gh-ps/js/chunk-7b18436e.b015dbf0.js"
+  },
+  {
+    "revision": "eb394bf39fb5dace095d",
+    "url": "/vue-widget_gh-ps/js/chunk-vendors.ce2e90f3.js"
+  },
+  {
+    "revision": "0b0bd6704b4f627646535c4b573cf92f",
+    "url": "/vue-widget_gh-ps/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-widget_gh-ps/robots.txt"
+  }
+]);
